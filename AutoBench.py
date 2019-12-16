cca9b91d5ff8ea8f58f9f30d7fb5e826323bd306
임근석 <rmstjr1030@nate.com>
2019-12-16 15:01:43 +0000

    Update Code
    CPU Code Update

--- AutoBench.py
+++ AutoBench.py
@@ -27,87 +27,107 @@ def help_print():
     print("         [midlow_range_gpus]<Print the 3st GPU Info>")
     print("         [low_end_gpus]<Print the 4st GPU Info>")
 
+def test_print():
+    res = requests.get('https://www.videocardbenchmark.net/high_end_gpus.html')
+    # res = requests.get('https://www.cpubenchmark.net/high_end_cpus.html')
+    soup = BeautifulSoup(res.content, 'lxml')
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
+    f = open("new.csv", 'w+', encoding='UTF8')
+    f.write(data)
+    f.close()
+    make_csv_new_g('new')
+    
+    # make_csv_new('new')    
+    
 def all_print():
     items = list(range(0,100))
     l = len(items)
     printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
     res = requests.get('https://www.cpubenchmark.net/high_end_cpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("high_end_cpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv_c('high_end_cpus')
+    make_csv_new('high_end_cpus')
+    # make_csv_c('high_end_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '1)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.cpubenchmark.net/mid_range_cpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("mid_range_cpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv_c('mid_range_cpus')
+    make_csv_new('mid_range_cpus')
+    # make_csv_c('mid_range_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '2)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.cpubenchmark.net/midlow_range_cpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("midlow_range_cpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv_c('midlow_range_cpus')
+    make_csv_new('midlow_range_cpus')
+    # make_csv_c('midlow_range_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '3)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.cpubenchmark.net/low_end_cpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("low_end_cpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv_c('low_end_cpus')
+    make_csv_new('low_end_cpus')
+    # make_csv_c('low_end_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '4)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.videocardbenchmark.net/high_end_gpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("high_end_gpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv('high_end_gpus')
+    make_csv_new_g('high_end_gpus')
+    # make_csv('high_end_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '5)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.videocardbenchmark.net/mid_range_gpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("mid_range_gpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv('mid_range_gpus')
+    make_csv_new_g('mid_range_gpus')
+    # make_csv('mid_range_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '6)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.videocardbenchmark.net/midlow_range_gpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("midlow_range_gpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv('midlow_range_gpus')
+    make_csv_new_g('midlow_range_gpus')
+    # make_csv('midlow_range_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '7)Progress:', suffix = 'Running', length = 50)
     res = requests.get('https://www.videocardbenchmark.net/low_end_gpus.html')
     soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("table",{"class": "chart"}).get_text()
+    data = soup.find("ul",{"class": "chartlist"}).get_text()
     f = open("low_end_gpus.csv", 'w+', encoding='UTF8')
     f.write(data)
     f.close()
-    make_csv('low_end_gpus')
+    make_csv_new_g('low_end_gpus')
+    # make_csv('low_end_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '8)Progress:', suffix = 'Running', length = 50)
@@ -125,11 +145,12 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.cpubenchmark.net/high_end_cpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("high_end_cpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
-        make_csv_c('high_end_cpus')
+        make_csv_new('high_end_cpus')
+        # make_csv_c('high_end_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -140,11 +161,12 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.cpubenchmark.net/mid_range_cpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("mid_range_cpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
-        make_csv_c('mid_range_cpus')
+        make_csv_new('mid_range_cpus')
+        # make_csv_c('mid_range_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -155,11 +177,12 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.cpubenchmark.net/midlow_range_cpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("midlow_range_cpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
-        make_csv_c('midlow_range_cpus')
+        make_csv_new('midlow_range_cpus')
+        # make_csv_c('midlow_range_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -170,11 +193,12 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.cpubenchmark.net/low_end_cpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("low_end_cpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
-        make_csv_c('low_end_cpus')
+        make_csv_new('low_end_cpus')
+        # make_csv_c('low_end_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -185,7 +209,7 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.videocardbenchmark.net/high_end_gpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("high_end_gpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
@@ -200,7 +224,7 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.videocardbenchmark.net/mid_range_gpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("mid_range_gpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
@@ -215,7 +239,7 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.videocardbenchmark.net/midlow_range_gpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("midlow_range_gpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
@@ -230,7 +254,7 @@ def input_url(url):
         printProgress(0, 1, prefix = 'Progress:', suffix = 'Wait...', length = 50)
         res = requests.get('https://www.videocardbenchmark.net/low_end_gpus.html')
         soup = BeautifulSoup(res.content, 'lxml')
-        data = soup.find("table",{"class": "chart"}).get_text()
+        data = soup.find("ul",{"class": "chartlist"}).get_text()
         f = open("low_end_gpus.csv", 'w+', encoding='UTF8')
         f.write(data)
         f.close()
@@ -240,14 +264,11 @@ def input_url(url):
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
         print('Complete!!!')
 
-def make_csv(name):
+def make_csv_new(name):
     str = []
     tmp = []
-    tmp2 = []
-    tmp3 = []
-    tmp4 = []
-    tmp5 = []
-
+    test = ""
+    
     f = open(name+".csv", 'r', encoding='UTF8')
     lines = f.readlines()
     for line in lines[5:-2]:
@@ -255,76 +276,151 @@ def make_csv(name):
     f.close()
 
     for i in str:
-        if "," in i:
-            tmp.append(i.replace(',',''))
-        else:
-            tmp.append(i)
-    for i in tmp:
+        i = i.replace(",","").strip()
         if "NA" in i:
-            tmp2.append('')
-        else:
-            tmp2.append(i)
-    for i in tmp2:
-        if "$" in i:
-            tmp3.append('')
-        else:
-            tmp3.append(i)
-    for i in tmp3:
-        if "   " in i:
-            tmp4.append(i.replace('   ', ', '))
-        else:
-            tmp4.append(i)
-    for i in tmp4:
-        if "  " in i:
-            tmp5.append(i.replace('  ', ''))
-        else:
-            tmp5.append(i)
-
+            i = i[:i.find("NA")]
+            if i[i.find('%') - 2] != '(':#두자리 수
+                tmp.append(i[:i.index('%') - 3] + "," + i[i.index('%') + 2:])
+            elif i[i.find('%') - 2] == '(':#한자리 수
+                tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
+        elif "$" in i:
+            i = i[:i.find('$')]
+            if i[i.find('%') - 2] != '(':#두자리 수
+                tmp.append(i[:i.index('%') - 3] + "," + i[i.index('%') + 2:])
+            elif i[i.find('%') - 2] == '(':#한자리 수
+                tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
+                
     f = open(name+".csv", 'w+', encoding='UTF8')
-    for i in tmp4:
+    for i in tmp:
         f.write(i)
+        f.write("\n")
 
     f.close()
-
-def make_csv_c(name):
+     
+def make_csv_new_g(name):
     str = []
     tmp = []
-    tmp2 = []
-    tmp3 = []
-    tmp4 = []
-
+    test = ""
+    
     f = open(name+".csv", 'r', encoding='UTF8')
     lines = f.readlines()
-    for line in lines[5:-2]:
+    for line in lines[:]:
         str.append(line)
     f.close()
-
+    
     for i in str:
-        if "," in i:
-            tmp.append(i.replace(',',''))
-        else:
-            tmp.append(i)
-    for i in tmp:
-        if "       " in i:
-            tmp2.append(i.replace('       ', ', '))
-        else:
-            tmp2.append(i)
-    for i in tmp2:
-        if "    " in i:
-            tmp3.append(i.replace('    ', ', '))
-        else:
-            tmp3.append(i)
-    for i in tmp3:
-        if "  " in i:
-            tmp4.append(i.replace('  ', ''))
-        else:
-            tmp4.append(i)
+        if len(i) > 1:
+            tmp.append(i.strip() + ",")
+            
+            
+    # for i in str:
+    #     i = i.replace(",","").strip()
+    #     if "NA" in i:
+    #         i = i[:i.find("NA")]
+    #         if i[i.find('%') - 2] != '(':#두자리 수
+    #             tmp.append(i[:i.index('%') - 3] + "," + i[i.index('%') + 2:])
+    #         elif i[i.find('%') - 2] == '(':#한자리 수
+    #             tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
+    #     elif "$" in i:
+    #         i = i[:i.find('$')]
+    #         if i[i.find('%') - 2] != '(':#두자리 수
+    #             tmp.append(i[:i.index('%') - 3] + "," + i[i.index('%') + 2:])
+    #         elif i[i.find('%') - 2] == '(':#한자리 수
+    #             tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
+                
     f = open(name+".csv", 'w+', encoding='UTF8')
-    for i in tmp4:
+    for i in tmp:
         f.write(i)
-    f.close()
+        f.write("\n")
+
+    f.close()        
+
+# def make_csv(name):
+#     str = []
+#     tmp = []
+#     tmp2 = []
+#     tmp3 = []
+#     tmp4 = []
+#     tmp5 = []
+
+#     f = open(name+".csv", 'r', encoding='UTF8')
+#     lines = f.readlines()
+#     for line in lines[0:]:
+#         str.append(line)
+#     f.close()
+
+#     for i in str:
+#         if "," in i:
+#             tmp.append(i.replace(',',''))
+#         else:
+#             tmp.append(i)
+#     for i in tmp:
+#         if "NA" in i:
+#             tmp2.append('')
+#         else:
+#             tmp2.append(i)
+#     for i in tmp2:
+#         if "$" in i:
+#             tmp3.append('')
+#         else:
+#             tmp3.append(i)
+#     for i in tmp3:
+#         if "   " in i:
+#             tmp4.append(i.replace('   ', ', '))
+#         else:
+#             tmp4.append(i)
+#     for i in tmp4:
+#         if "  " in i:
+#             tmp5.append(i.replace('  ', ''))
+#         else:
+#             tmp5.append(i)
+
+#     f = open(name+".csv", 'w+', encoding='UTF8')
+#     for i in tmp4:
+#         f.write(i)
+
+#     f.close()
+
+# def make_csv_c(name):
+#     str = []
+#     tmp = []
+#     tmp2 = []
+#     tmp3 = []
+#     tmp4 = []
+
+#     f = open(name+".csv", 'r', encoding='UTF8')
+#     lines = f.readlines()
+#     for line in lines[5:-2]:
+#         str.append(line)
+#     f.close()
+
+#     for i in str:
+#         if "," in i:
+#             tmp.append(i.replace(',',''))
+#         else:
+#             tmp.append(i)
+#     for i in tmp:
+#         if "       " in i:
+#             tmp2.append(i.replace('       ', ', '))
+#         else:
+#             tmp2.append(i)
+#     for i in tmp2:
+#         if "    " in i:
+#             tmp3.append(i.replace('    ', ', '))
+#         else:
+#             tmp3.append(i)
+#     for i in tmp3:
+#         if "  " in i:
+#             tmp4.append(i.replace('  ', ''))
+#         else:
+#             tmp4.append(i)
+#     f = open(name+".csv", 'w+', encoding='UTF8')
+#     for i in tmp4:
+#         f.write(i)
+#     f.close()
 
 if len(sys.argv) == 1:
-    all_print()
+    test_print()
+    # all_print()
 else:
     input_url(sys.argv[1])
