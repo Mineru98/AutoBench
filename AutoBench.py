af4878d28140e4e9741e2eda6002cbb7b462ae12
임근석 <rmstjr1030@nate.com>
2019-12-17 03:46:59 +0000

    UpdateCode
    Update GPU Code

--- AutoBench.py
+++ AutoBench.py
@@ -27,18 +27,6 @@ def help_print():
     print("         [midlow_range_gpus]<Print the 3st GPU Info>")
     print("         [low_end_gpus]<Print the 4st GPU Info>")
 
-def test_print():
-    res = requests.get('https://www.videocardbenchmark.net/high_end_gpus.html')
-    # res = requests.get('https://www.cpubenchmark.net/high_end_cpus.html')
-    soup = BeautifulSoup(res.content, 'lxml')
-    data = soup.find("ul",{"class": "chartlist"}).get_text()
-    f = open("new.csv", 'w+', encoding='UTF8')
-    f.write(data)
-    f.close()
-    make_csv_new_g('new')
-    
-    # make_csv_new('new')    
-    
 def all_print():
     items = list(range(0,100))
     l = len(items)
@@ -50,7 +38,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new('high_end_cpus')
-    # make_csv_c('high_end_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '1)Progress:', suffix = 'Running', length = 50)
@@ -61,7 +48,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new('mid_range_cpus')
-    # make_csv_c('mid_range_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '2)Progress:', suffix = 'Running', length = 50)
@@ -72,7 +58,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new('midlow_range_cpus')
-    # make_csv_c('midlow_range_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '3)Progress:', suffix = 'Running', length = 50)
@@ -83,7 +68,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new('low_end_cpus')
-    # make_csv_c('low_end_cpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '4)Progress:', suffix = 'Running', length = 50)
@@ -94,7 +78,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new_g('high_end_gpus')
-    # make_csv('high_end_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '5)Progress:', suffix = 'Running', length = 50)
@@ -105,7 +88,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new_g('mid_range_gpus')
-    # make_csv('mid_range_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '6)Progress:', suffix = 'Running', length = 50)
@@ -116,7 +98,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new_g('midlow_range_gpus')
-    # make_csv('midlow_range_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '7)Progress:', suffix = 'Running', length = 50)
@@ -127,7 +108,6 @@ def all_print():
     f.write(data)
     f.close()
     make_csv_new_g('low_end_gpus')
-    # make_csv('low_end_gpus')
     for i, item in enumerate(items):
         sleep(0.01)
         printProgress(i+1, l, prefix = '8)Progress:', suffix = 'Running', length = 50)
@@ -150,7 +130,6 @@ def input_url(url):
         f.write(data)
         f.close()
         make_csv_new('high_end_cpus')
-        # make_csv_c('high_end_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -166,7 +145,6 @@ def input_url(url):
         f.write(data)
         f.close()
         make_csv_new('mid_range_cpus')
-        # make_csv_c('mid_range_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -182,7 +160,6 @@ def input_url(url):
         f.write(data)
         f.close()
         make_csv_new('midlow_range_cpus')
-        # make_csv_c('midlow_range_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -198,7 +175,6 @@ def input_url(url):
         f.write(data)
         f.close()
         make_csv_new('low_end_cpus')
-        # make_csv_c('low_end_cpus')
         for i, item in enumerate(items):
             sleep(0.01)
             printProgress(i+1, l, prefix = 'Progress:', suffix = 'Running', length = 50)
@@ -290,6 +266,7 @@ def make_csv_new(name):
             elif i[i.find('%') - 2] == '(':#한자리 수
                 tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
                 
+
     f = open(name+".csv", 'w+', encoding='UTF8')
     for i in tmp:
         f.write(i)
@@ -300,7 +277,7 @@ def make_csv_new(name):
 def make_csv_new_g(name):
     str = []
     tmp = []
-    test = ""
+    count = 1
     
     f = open(name+".csv", 'r', encoding='UTF8')
     lines = f.readlines()
@@ -310,117 +287,30 @@ def make_csv_new_g(name):
     
     for i in str:
         if len(i) > 1:
-            tmp.append(i.strip() + ",")
-            
-            
-    # for i in str:
-    #     i = i.replace(",","").strip()
-    #     if "NA" in i:
-    #         i = i[:i.find("NA")]
-    #         if i[i.find('%') - 2] != '(':#두자리 수
-    #             tmp.append(i[:i.index('%') - 3] + "," + i[i.index('%') + 2:])
-    #         elif i[i.find('%') - 2] == '(':#한자리 수
-    #             tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
-    #     elif "$" in i:
-    #         i = i[:i.find('$')]
-    #         if i[i.find('%') - 2] != '(':#두자리 수
-    #             tmp.append(i[:i.index('%') - 3] + "," + i[i.index('%') + 2:])
-    #         elif i[i.find('%') - 2] == '(':#한자리 수
-    #             tmp.append(i[:i.index('%') - 2] + "," + i[i.index('%') + 2:])
-                
+            i = i.replace(",","").strip()
+            if count%3 == 1:
+                tmp.append(i)
+                count+=1
+            elif count%3 == 2:
+                tmp.append(i[i.find(')') + 2:])
+                count+=1
+            elif count%3 == 0:
+                count+=1
+
+    count = 1
+
     f = open(name+".csv", 'w+', encoding='UTF8')
     for i in tmp:
-        f.write(i)
-        f.write("\n")
-
-    f.close()        
-
-# def make_csv(name):
-#     str = []
-#     tmp = []
-#     tmp2 = []
-#     tmp3 = []
-#     tmp4 = []
-#     tmp5 = []
-
-#     f = open(name+".csv", 'r', encoding='UTF8')
-#     lines = f.readlines()
-#     for line in lines[0:]:
-#         str.append(line)
-#     f.close()
-
-#     for i in str:
-#         if "," in i:
-#             tmp.append(i.replace(',',''))
-#         else:
-#             tmp.append(i)
-#     for i in tmp:
-#         if "NA" in i:
-#             tmp2.append('')
-#         else:
-#             tmp2.append(i)
-#     for i in tmp2:
-#         if "$" in i:
-#             tmp3.append('')
-#         else:
-#             tmp3.append(i)
-#     for i in tmp3:
-#         if "   " in i:
-#             tmp4.append(i.replace('   ', ', '))
-#         else:
-#             tmp4.append(i)
-#     for i in tmp4:
-#         if "  " in i:
-#             tmp5.append(i.replace('  ', ''))
-#         else:
-#             tmp5.append(i)
-
-#     f = open(name+".csv", 'w+', encoding='UTF8')
-#     for i in tmp4:
-#         f.write(i)
-
-#     f.close()
-
-# def make_csv_c(name):
-#     str = []
-#     tmp = []
-#     tmp2 = []
-#     tmp3 = []
-#     tmp4 = []
-
-#     f = open(name+".csv", 'r', encoding='UTF8')
-#     lines = f.readlines()
-#     for line in lines[5:-2]:
-#         str.append(line)
-#     f.close()
-
-#     for i in str:
-#         if "," in i:
-#             tmp.append(i.replace(',',''))
-#         else:
-#             tmp.append(i)
-#     for i in tmp:
-#         if "       " in i:
-#             tmp2.append(i.replace('       ', ', '))
-#         else:
-#             tmp2.append(i)
-#     for i in tmp2:
-#         if "    " in i:
-#             tmp3.append(i.replace('    ', ', '))
-#         else:
-#             tmp3.append(i)
-#     for i in tmp3:
-#         if "  " in i:
-#             tmp4.append(i.replace('  ', ''))
-#         else:
-#             tmp4.append(i)
-#     f = open(name+".csv", 'w+', encoding='UTF8')
-#     for i in tmp4:
-#         f.write(i)
-#     f.close()
+        if count%2 == 0:
+            f.write(i)
+            f.write("\n")
+            count+=1
+        else:
+            f.write(i + ",")
+            count+=1
+    f.close()       
 
 if len(sys.argv) == 1:
-    test_print()
-    # all_print()
+    all_print()
 else:
     input_url(sys.argv[1])
